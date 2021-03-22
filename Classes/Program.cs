41184be3ff9dde371a7b7685c1846b1d6f96210c
James Savelli-Holt <James.Savelli-Holt@cerillion.com>
2021-03-22 14:27:54 +0000

    Update

---
+++ Classes/Program.cs
@@ -0,0 +1,38 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace TimeClass
+{
+    public class Time
+    {
+        //private variables
+        int Year;
+        int Month;
+        int Date;
+        int Hour;
+        int Minute;
+        int Second;
+
+        //public methods
+        public void DisplayCurrentTime()
+        {
+            Console.WriteLine(
+                "stub for DisplayCurrentTime");
+        }
+    }
+public class Tester
+    {
+        static void Main()
+        {
+            Time t = new Time();
+            t.DisplayCurrentTime();
+        }
+
+    }
+
+}
